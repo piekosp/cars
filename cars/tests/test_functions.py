0ed1f9d74dc30b8273ab1b81e4dddc5c26d6d517
Paweł Piękoś <71713477+piekosp@users.noreply.github.com>
2021-12-14 16:40:20 +0100

    Update test_functions.py

--- cars/tests/test_functions.py
+++ cars/tests/test_functions.py
@@ -6,7 +6,7 @@ from cars.functions import check_if_car_exists
 class TestCarExistance(TestCase):
 
     def test_car_exists(self):
-        self.assertTrue(check_if_car_exists('BMW', 'M3'))
+        self.assertTrue(check_if_car_exists('BMW', 'M7'))
 
     def test_car_does_not_exists(self):
         self.assertFalse(check_if_car_exists('AUDI', 'M3'))
